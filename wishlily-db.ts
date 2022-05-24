0d7cce365b071efc0545c1e14727e5a26989938b
Sam Knight <robogamingbysam@icloud.com>
2022-05-24 08:45:22 -0500

    Actually respond with document and not OID

--- wishlily-db.ts
+++ wishlily-db.ts
@@ -59,7 +59,7 @@ router.post('/add_item_to_wishlist', async (ctx) => {
     throw new Error('User Key does not match!')
   }
 
-  const doc = (await mongo.database('wishlily').collection(`wishes`).insertOne({
+  const oid = (await mongo.database('wishlily').collection(`wishes`).insertOne({
     userId,
     wishlistId,
     cover,
@@ -69,7 +69,7 @@ router.post('/add_item_to_wishlist', async (ctx) => {
   }))
 
   ctx.response.status = 200
-  ctx.response.body = { embed: { id: doc._id.toString(), ...doc }, success: true}
+  ctx.response.body = { embed: { id: oid.toString(), cover, title, price, link }, success: true}
 })
 
 router.post('/confirm_user', async (ctx) => {
@@ -114,58 +114,66 @@ router.post('/create_user', async (ctx) => {
 })
 
 router.post('/create_wishlist', async (ctx) => {
-  const json = await ctx.request.body({type: 'json'}).value
+  try {
+    const json = await ctx.request.body({type: 'json'}).value
 
-  const userId = json.userId.toString()
-  // Matches a UUID but NOT the nil UUID
-  if (!userId.match(/^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i)) {
-    throw new Error('User ID failed to validate')
+    console.log(json)
+
+    const userId = json.userId.toString()
+    // Matches a UUID but NOT the nil UUID
+    if (!userId.match(/^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i)) {
+      throw new Error('User ID failed to validate')
+    }
+
+    const userKey = (await mongo.database('wishlily').collection('users').findOne({
+      userId
+    }))?.['userKey']
+
+    if (userKey !== json.userKey || userKey === undefined) {
+      throw new Error('User Key does not match!')
+    }
+
+    if (!json.title) throw new Error('Title is required.')
+
+    const color = json?.color?.toString() ?? '#ffffff'
+    if (!color.match(/^#([0-9a-f]{3}){1,2}$/i)) {
+      throw new Error('color must be a valid hex color')
+    }
+
+    if (json.title.toString().length > 2048) {
+      throw new Error('Title too long.')
+    }
+
+    if (json.address !== undefined && json.address.toString().length > 8192) {
+      throw new Error('Address too long.')
+    }
+
+    const address = json.address === undefined ? undefined : json.address.toString().substr(0, 8192)
+
+    if (address !== undefined && !address.match(/client_side_enc,[A-Fa-f0-9]*,[A-Fa-f0-9]{32}/i)) {
+      throw new Error('Encryption failed. (field: address)')
+    }
+
+    const title = json.title.toString().substr(0, 2048)
+
+    if (title === undefined || !title.match(/client_side_enc,[A-Fa-f0-9]*,[A-Fa-f0-9]{32}/i)) {
+      throw new Error('Encryption failed. (field: title)')
+    }
+
+    const wishlist = (await mongo.database('wishlily').collection(`user_wishlists`).insertOne({
+        userId,
+        title,
+        address,
+        color
+    }))
+
+    ctx.response.status = 200
+    ctx.response.body = { wishlist: { id: wishlist.toString(), color, title, address }, success: true}
+  } catch (e) {
+    ctx.response.status = 500
+    ctx.response.body = e.message
+    return
   }
-
-  const userKey = (await mongo.database('wishlily').collection('users').findOne({
-    userId
-  }))?.['userKey']
-
-  if (userKey !== json.userKey || userKey === undefined) {
-    throw new Error('User Key does not match!')
-  }
-
-  if (!json.title) throw new Error('Title is required.')
-
-  const color = json?.color?.toString() ?? '#ffffff'
-  if (!color.match(/^#([0-9a-f]{3}){1,2}$/i)) {
-    throw new Error('color must be a valid hex color')
-  }
-
-  if (json.title.toString().length > 2048) {
-    throw new Error('Title too long.')
-  }
-
-  if (json.address !== undefined && json.address.toString().length > 8192) {
-    throw new Error('Address too long.')
-  }
-
-  const address = json.address === undefined ? undefined : json.address.toString().substr(0, 8192)
-
-  if (address !== undefined && !address.match(/client_side_enc,[A-Fa-f0-9]*,[A-Fa-f0-9]{32}/i)) {
-    throw new Error('Encryption failed. (field: address)')
-  }
-
-  const title = json.title.toString().substr(0, 2048)
-
-  if (title === undefined || !title.match(/client_side_enc,[A-Fa-f0-9]*,[A-Fa-f0-9]{32}/i)) {
-    throw new Error('Encryption failed. (field: title)')
-  }
-
-  const wishlist = (await mongo.database('wishlily').collection(`user_wishlists`).insertOne({
-      userId,
-      title,
-      address,
-      color
-  }))
-
-  ctx.response.status = 200
-  ctx.response.body = { wishlist: { id: wishlist._id.toString(), ...wishlist }, success: true}
 })
 
 router.post('/delete_item_from_wishlist', async (ctx) => {
@@ -372,6 +380,7 @@ router.post('/get_wishlist_info', async (ctx) => {
 
   ctx.response.status = 200
   ctx.response.body = {
+    id: doc?._id?.toString(),
     title: doc?.title,
     address: doc?.address,
     color: doc?.color
