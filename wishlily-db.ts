5397b7ac0dd59f16ee62d4fd7f95295cd57366c9
Sam Knight <robogamingbysam@icloud.com>
2022-05-24 08:17:39 -0500

    Allow * for prod (fetch errors)

--- wishlily-db.ts
+++ wishlily-db.ts
@@ -462,7 +462,7 @@ router.get('/', async (ctx) => {
 })
 
 const app = new Application()
-app.use(CORS({origin: (Deno.env.get('ENVIRONMENT') === 'production' ? 'wishlily.app' : '*')}))
+app.use(CORS({origin: '*'}))
 app.use(router.routes())
 app.use(router.allowedMethods())
 
